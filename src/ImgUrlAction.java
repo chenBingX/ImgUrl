8062564c4dbbd7cbc39e504a6c9d2de65300c2b4
纽特 <coorchice.cb@alibaba-inc.com>
2018-10-18 17:36:42 +0800

    更新

--- src/ImgUrlAction.java
+++ src/ImgUrlAction.java
@@ -21,31 +21,40 @@ public class ImgUrlAction extends com.intellij.openapi.actionSystem.AnAction {
         VirtualFile selectedFile = DataKeys.VIRTUAL_FILE.getData(event.getDataContext());
         if (project != null && selectedFile != null) {
             String path1 = "https://raw.githubusercontent.com/";
-            String path2 = getPath2();
-            String path3 = getPath3(project, selectedFile);
-            String imgUrl = path1 + path2 + path3;
-            CopyPasteManager.getInstance().setContents(new StringSelection(imgUrl));
+            String path2 = getPath2(project);
+            if (path2 != null){
+                String path3 = getPath3(project, selectedFile);
+                String imgUrl = path1 + path2 + path3;
+                CopyPasteManager.getInstance().setContents(new StringSelection(imgUrl));
+                Notifications.Bus.notify(new Notification(
+                        project.getName()
+                        , project.getName()
+                        , "url已复制到剪切板!  ->  " + imgUrl
+                        , NotificationType.INFORMATION));
+            }
+        }
+    }
+
+    private String getPath2(Project project) {
+        String projPath = project.getBasePath();
+        File userNameFile = new File(projPath + "/username.txt");
+        if (userNameFile.exists()) {
+            VirtualFile userNameVF = LocalFileSystem.getInstance().findFileByIoFile(userNameFile);
+            Document document = FileDocumentManager.getInstance().getDocument(userNameVF);
+            return document.getText();
+        } else {
             Notifications.Bus.notify(new Notification(
                     project.getName()
                     , project.getName()
-                    , "复制路径成功!  ->  " + imgUrl
+                    , "需要先在项目根目录创建 username.txt 文件，然后写入 github 的用户昵称，就是每个仓库前的那个名称。多的什么都不要多写！"
                     , NotificationType.INFORMATION));
+            return null;
         }
-    }
-
-    private String getPath2() {
-        File userNameFile = new File("username.txt");
-        if (!userNameFile.exists()) {
-            userNameFile.mkdir();
-        }
-        VirtualFile userNameVF = LocalFileSystem.getInstance().findFileByIoFile(userNameFile);
-        Document document = FileDocumentManager.getInstance().getDocument(userNameVF);
-        return document.getText();
     }
 
     private String getPath3(Project project, VirtualFile selectedFile) {
-        int startIndex = project.getBasePath().length() - project.getName().length() - 1;
-        String path3 = selectedFile.getPath().substring(startIndex);
+        int startIndex = project.getBasePath().length();
+        String path3 = "/" + project.getName() + "/master" + selectedFile.getPath().substring(startIndex);
         return path3;
     }
 
